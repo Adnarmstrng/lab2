332cc2657a0b0a252431cc40acc00eaa69107f04
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-02-12 20:38:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,37 +8,28 @@ const map = new mapboxgl.Map({
 
 map.on('load', () => {
 //Add a data source containing GeoJSON data
-map.addSource('uoft-data', {
-    type: 'geojson',
-    data: {
-    "type": "FeatureCollection",
-    "features": [
-    {
-    "type": "Feature",
-    "properties": {
-    "name": "Sidney Smith Hall"
-    },
-    "geometry": {
-    "coordinates": [
-    -79.39865237301687,
-    43.662343395037766
-    ],
-    "type": "Point"
-    }
-    }
-    ]
-    }
+
+// Add a data source from a Mapbox tileset
+map.addSource('censustracts', { // Create your own source ID
+    'type': 'vector',
+    'url': 'mapbox://aidanarmstrong.a2ueu202' // Update to your mapbox tileset ID
     });
     map.addLayer({
-    'id': 'uoft-pnt',
-    'type': 'circle',
-    'source': 'uoft-data',
+    'id': 'map-census', // Create your own layer ID
+    'type': 'fill', // Note this is different to point data
+    'source': 'censustracts', // Must match source ID from addSource Method
     'paint': {
-    'circle-radius': 6,
-    'circle-color': '#B42222'
-    }
-    });
-    });
+    'fill-color': '#888888', // Test alternative colours and style properties
+    'fill-opacity': 0.4,
+    'fill-outline-color': 'black'
+    },
+    'source-layer': '...' // Tileset NAME (diff to ID), get this from mapbox tileset page
+    },
+    // Drawing order - places layer below points
+    // Here the addlayer method takes 2 arguments (the layer as an object and a
+    //string for another layer's name). If the other layer already exists, the new layer
+    //will be drawn before that one
+    )})
 
 
 // map.on('load', () => {

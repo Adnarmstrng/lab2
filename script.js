225c23dad19249be9b858e1971fc0affd9ba876c
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-02-12 20:20:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const map = new mapboxgl.Map({
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
    
-    map.addSource('earthquakes', {
+    map.addSource('parks', {
     type: 'geojson',
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-    data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+    data: 'https://github.com/Adnarmstrng/lab2/main/parks.geojson',
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)

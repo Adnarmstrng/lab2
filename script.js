b983b2f2b9973c51e5d4588a8962b28381e2e162
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-02-12 20:16:34 -0500

    Added code for lab 2

---
+++ script.js
@@ -0,0 +1,75 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWlkYW5hcm1zdHJvbmciLCJhIjoiY2xzam1tczhoMnJqMzJpbzZ4OWh0bmI4dyJ9.2pEogvW_3XlwcsMh4kMfCQ'; //Add default public map token from your Mapbox account
+const map = new mapboxgl.Map({
+    container: 'my-map', // map container ID
+    style: 'mapbox://styles/mapbox/dark-v11', // style URL
+    center: [-79.384, 43.713], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+map.on('load', () => {
+    // Add a new source from our GeoJSON data and
+   
+    map.addSource('earthquakes', {
+    type: 'geojson',
+    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+    // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+    data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+    cluster: true,
+    clusterMaxZoom: 14, // Max zoom to cluster points on
+    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    });
+     
+    map.addLayer({
+    id: 'clusters',
+    type: 'circle',
+    source: 'parks',
+    filter: ['has', 'point_count'],
+    paint: {
+    //   * Blue, 20px circles when point count is less than 100
+    //   * Yellow, 30px circles when point count is between 100 and 750
+    //   * Pink, 40px circles when point count is greater than or equal to 750
+    'circle-color': [
+    'step',
+    ['get', 'point_count'],
+    '#51bbd6',
+    100,
+    '#f1f075',
+    750,
+    '#f28cb1'
+    ],
+    'circle-radius': [
+    'step',
+    ['get', 'point_count'],
+    20,
+    100,
+    30,
+    750,
+    40
+    ]
+    }
+    });
+     
+    map.addLayer({
+    id: 'cluster-count',
+    type: 'symbol',
+    source: 'parks',
+    filter: ['has', 'point_count'],
+    layout: {
+    'text-field': ['get', 'point_count_abbreviated'],
+    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+    'text-size': 12
+    }
+    });
+     
+    map.addLayer({
+    id: 'unclustered-point',
+    type: 'circle',
+    source: 'parks',
+    filter: ['!', ['has', 'point_count']],
+    paint: {
+    'circle-color': '#11b4da',
+    'circle-radius': 4,
+    'circle-stroke-width': 1,
+    'circle-stroke-color': '#fff'
+    }
+    })
+})
